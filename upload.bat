51fb7bc397160400984bd975f01b4541ae74e93c
Peter-Highness <1686532070@qq.com>
2020-10-18 14:06:14 +0800

    20201018140614

--- upload.bat
+++ upload.bat
@@ -1,3 +1,4 @@
-git add -A
+git add .
 git commit -m"%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%"
-git push -u origin master -f
+git pull origin master
+git push -u origin master
